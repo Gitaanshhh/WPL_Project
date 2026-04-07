0ec2ec3dc00fee819b71bf51a5d195a23c4df51f
Gitaanshhh <gitaanshbhuradia008@gmail.com>
2026-04-07 11:10:11 +0530

    Update main.jsx with Analytics and SpeedInsights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
 import './index.css'
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('app')).render(
         <QueryClientProvider client={queryClient}>
             <App />
             <Analytics />
+            <SpeedInsights />
         </QueryClientProvider>
     </React.StrictMode>,
 )
